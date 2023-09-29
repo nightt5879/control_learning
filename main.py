6c79081b33187be092a694854726ae19e0044b4d
Nightt <87569709+nightt5879@users.noreply.github.com>
2023-09-29 13:18:31 +0800

    存档一下

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+
+# 初始化摄像头
+cap = cv2.VideoCapture(0)
+
+while True:
+    # 从摄像头读取帧
+    ret, frame = cap.read()
+
+    # 显示帧
+    cv2.imshow('Video Stream', frame)
+
+    # 按下 'Q' 键退出
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# 释放摄像头和销毁所有 OpenCV 窗口
+cap.release()
+cv2.destroyAllWindows()
---
+++ mian_control.py
@@ -0,0 +1,9 @@
+from control import shijue0
+
+if __name__ == '__main__':
+    a = shijue0.basicImg()
+    a.camera(0)
+    while True:
+        a.get_img()
+        a.show_image("test")
+        a.delay(1)
